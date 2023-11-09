c3176eb4bab594656eb94098766d09b9e3b5e82f
Alexander Gladkov <gladkov481@gmail.com>
2023-11-10 04:59:45 +0700

    Реализация добавления удаления
    Изменение стиля, возможность добавление/удаления

--- src/Spectr/ViewModel/CustomerViewModel.cs
+++ src/Spectr/ViewModel/CustomerViewModel.cs
@@ -1,19 +1,21 @@
 ﻿using Spectr.Model;
+using Spectr.Commands;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
+using System.Diagnostics;
 
 namespace Spectr.ViewModel
 {
     /// <summary>
     /// Модель представления клиентов с бд
     /// </summary>
-    public class CustomerViewModel : INotifyPropertyChanged
+    internal class CustomerViewModel : ViewModelBase
     {
-        private ObservableCollection<Customer> _customers;
 
+        private ObservableCollection<Customer> _customers;
+        private Customer _selectedCustomer;
         public ObservableCollection<Customer> Customers
         {
             get { return _customers; }
@@ -26,13 +28,18 @@ namespace Spectr.ViewModel
                 }
             }
         }
-
-        public CustomerViewModel()
+        public Customer SelectedCustomer
         {
-            LoadDataAsync();
+            get => _selectedCustomer;
+            set
+            {
+                if (Equals(value, _selectedCustomer)) return;
+                _selectedCustomer = value;
+                OnPropertyChanged();
+            }
         }
 
-        public async Task LoadDataAsync()
+        private async Task LoadDataAsync()
         {
             Customers = await LoadDataFromDatabaseAsync();
         }
@@ -67,11 +74,100 @@ namespace Spectr.ViewModel
             return customers;
         }
 
+        #region Добавление клиента
 
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected virtual void OnPropertyChanged(string propertyName)
+        public ICommand AddCustomerCommand { get; }
+
+        private bool CanAddCustomerCommandExecute(object parameter)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            // Логика, чтобы определить, можно ли выполнить добавление
+            return true;
+        }
+
+        private async void OnAddCustomerExecuted(object parameter)
+        {
+            if (SelectedCustomer != null)
+            {
+                await AddCustomerAsync(SelectedCustomer);
+                Customers.Add(SelectedCustomer);
+                SelectedCustomer = new Customer();
+                await LoadDataAsync(); // Обновите данные из базы после добавления клиента
+            }
+        }
+
+        public async Task AddCustomerAsync(Customer customer)
+        {
+            using (SqlConnection con = new SqlConnection(Properties.Settings.Default.con))
+            {
+                await con.OpenAsync();
+
+                using (SqlCommand command = new SqlCommand("INSERT INTO Customer " +
+                                                           "VALUES (@DocNumber, @CustomerFirstName, @CustomerSecontName, @CustomerPatronymic, @PhoneNumber, @EmailAdress)", con))
+                {
+                    command.Parameters.AddWithValue("@DocNumber", customer.DocNumber);
+                    command.Parameters.AddWithValue("@CustomerFirstName", customer.CustomerFirstName);
+                    command.Parameters.AddWithValue("@CustomerSecontName", customer.CustomerSecontName);
+                    command.Parameters.AddWithValue("@CustomerPatronymic", customer.CustomerPatronymic);
+                    command.Parameters.AddWithValue("@PhoneNumber", customer.PhoneNumber);
+                    command.Parameters.AddWithValue("@EmailAdress", customer.EmailAdress);
+
+                    await command.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        #endregion
+
+
+        #region Удаление клиента
+
+        public ICommand DeleteCustomerCommand { get;  }
+
+        private bool CanDeleteCustomerCommandExecute(object parameter)
+        {
+            // Логика, чтобы определить, можно ли выполнить удаление
+            return true;
+        }
+
+        private void OnDeleteCustomerExecuted(object parameter)
+        {
+
+            if (parameter is Customer customer)
+            {
+                DeleteCustomerAsync(customer);
+            }
+        }
+
+        public async Task DeleteCustomerAsync(Customer customer)
+        {
+            using (SqlConnection con = new SqlConnection(Properties.Settings.Default.con))
+            {
+                await con.OpenAsync();
+
+                using (SqlCommand command = new SqlCommand("DELETE FROM Customer WHERE CustomerID=@CustomerID", con))
+                {
+                    command.Parameters.AddWithValue("@CustomerID", customer.CustomerID);
+                    command.ExecuteNonQuery(); 
+                }
+            }
+
+            Customers.Remove(customer);
+            OnPropertyChanged(nameof(Customers));          
+        }
+
+        #endregion
+
+        public CustomerViewModel()
+        {
+            Customers = new ObservableCollection<Customer>();
+            SelectedCustomer = new Customer();
+
+            LoadDataAsync();
+
+
+            AddCustomerCommand = new LambdaCommand(OnAddCustomerExecuted, CanAddCustomerCommandExecute);
+
+            DeleteCustomerCommand = new LambdaCommand(OnDeleteCustomerExecuted, CanDeleteCustomerCommandExecute);      
         }
     }
 }
--- src/Spectr/ViewModel/MainWindowViewModel.cs
+++ src/Spectr/ViewModel/MainWindowViewModel.cs
@@ -10,15 +10,6 @@ namespace Spectr.ViewModel
 {
     internal class MainWindowViewModel : ViewModelBase
     {
-        private readonly CustomerViewModel _customerViewModel;
-
-        private ObservableCollection<Customer> _customers;
-        public ObservableCollection<Customer> Customers
-        {
-            get { return _customers; }
-            set { Set(ref _customers, value); }
-        }
-
 
         #region Визуалка
         #region Заголовок
@@ -46,7 +37,7 @@ namespace Spectr.ViewModel
         #endregion
 
 
-        #region Комманды
+        #region Общие комманды приложения
 
         #region Комманда закрытия приложения
         public ICommand CloseAppCommand { get; }
@@ -65,8 +56,10 @@ namespace Spectr.ViewModel
             if (p is Window window) window.DragMove();
         }
         #endregion
+      
 
         #endregion
+       
         public MainWindowViewModel()
         {
             #region Команды
@@ -74,21 +67,9 @@ namespace Spectr.ViewModel
             CloseAppCommand = new LambdaCommand(OnCloseAppCommandExecuted, CanCloseAppCommandExecute);
 
             DragWindowCommand = new LambdaCommand(OnDragWindowCommandExecuted, CanDragWindowCommandExecute);
-
-
-            #endregion
-
-            _customerViewModel = new CustomerViewModel();
-            Task.Run(async () => await InitializeDataAsync());
+           
+            #endregion           
         
-        
-        
-        }
-
-        private async Task InitializeDataAsync()
-        {
-            await _customerViewModel.LoadDataAsync();
-            Customers = _customerViewModel.Customers;
         }
     }
 }
