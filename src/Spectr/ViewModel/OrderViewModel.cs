b5ed096acdb002de9a33fdb6a94f0e978d242fbd
Alexander Gladkov <gladkov481@gmail.com>
2023-12-01 00:54:27 +0700

    AddOrderView done
    Доделано отображение окна добавления инфы в Заказы

--- src/Spectr/ViewModel/OrderViewModel.cs
+++ src/Spectr/ViewModel/OrderViewModel.cs
@@ -3,9 +3,12 @@ using Spectr.Model;
 using Spectr.Model.DataContext;
 using Spectr.View;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
@@ -52,18 +55,7 @@ namespace Spectr.ViewModel
             }
         }
 
-        public ObservableCollection<Employer> Employers
-        {
-            get { return _employers; }
-            set
-            {
-                if (_employers != value)
-                {
-                    _employers = value;
-                    OnPropertyChanged(nameof(Employers));
-                }
-            }
-        }
+        
 
         private Employer _selectedEmployer;
         public Employer SelectedEmployer
@@ -82,7 +74,8 @@ namespace Spectr.ViewModel
 
 
 
-        #region Отборажение данных        
+        #region Отборажение данных    
+        
         private DataTable _repairOrderDataTable;
         public DataTable RepairOrderDataTable
         {
@@ -118,7 +111,6 @@ namespace Spectr.ViewModel
                         "JOIN Device ON Device.DeviceID = Repair.DeviceID\r\nLEFT JOIN RepairCategoryJunction ON RepairCategoryJunction.OrderID = Repair.OrderID\r\n" +
                         "LEFT JOIN RepairCategory ON RepairCategoryJunction.CategoryID = RepairCategory.CategoryID\r\nGROUP BY Repair.OrderID, DateStart, PlainDateEnd, TotalCost,\r\n" +
                         "Customer.CustomerSecondName, Customer.CustomerFirstName, Customer.CustomerPatronymic,\r\nSerialNumber, EmFirstName, EmSecondName, Comment", con))
-
                     using (SqlDataReader reader = await command.ExecuteReaderAsync())
                     {
                         dataTable.Load(reader);
@@ -133,19 +125,81 @@ namespace Spectr.ViewModel
             return dataTable;
         }
 
-
-
-        private async Task EmLoadDataAsync()
+        private DataTable _employerDataTable;
+        public DataTable EmployerDataTable
         {
-            Employers = await EmLoadDataFromDatabaseAsync();
-            OnPropertyChanged(nameof(Employers));
+            get { return _employerDataTable; }
+            set
+            {
+                _employerDataTable = value;
+                OnPropertyChanged(nameof(EmployerDataTable));
+            }
+        }
+
+        private DataTable _employerDataTableComboBox;
+        public DataTable EmployerDataTableComboBox
+        {
+            get { return _employerDataTableComboBox; }
+            set
+            {
+                _employerDataTableComboBox = value;
+                OnPropertyChanged(nameof(EmployerDataTableComboBox));
+            }
+        }
+
+        private DataTable _customerDataTable;
+        public DataTable CustomerDataTable
+        {
+            get { return _customerDataTable; }
+            set
+            {
+                _customerDataTable = value;
+                OnPropertyChanged(nameof(CustomerDataTable));
+            }
+        }
+
+        private DataTable _deviceDataTable;
+        public DataTable DeviceDataTable
+        {
+            get { return _deviceDataTable; }
+            set
+            {
+                _deviceDataTable = value;
+                OnPropertyChanged(nameof(DeviceDataTable));
+            }
         }
 
 
-
-        private async Task<ObservableCollection<Employer>> EmLoadDataFromDatabaseAsync()
+        private async Task LoadAllDataAsync()
         {
-            ObservableCollection<Employer> employers = new ObservableCollection<Employer>();
+
+            List<DataTable> dataTablesLoaded = await LoadDataFromDatabaseAsync();
+
+            
+            EmployerDataTable = dataTablesLoaded[0];
+
+    
+            DataTable partialDataTable = dataTablesLoaded[0];
+            foreach (DataColumn column in partialDataTable.Columns.Cast<DataColumn>().ToArray())
+            {
+                if (column.ColumnName != "ID" && column.ColumnName != "Работник")
+                {
+                    partialDataTable.Columns.Remove(column);
+                }
+            }
+           
+            EmployerDataTableComboBox = partialDataTable;
+
+            CustomerDataTable = dataTablesLoaded[1];
+
+            DeviceDataTable = dataTablesLoaded[2];
+        }
+
+
+        public async Task<List<DataTable>> LoadDataFromDatabaseAsync()
+        {
+            //DataTable dataTable = new DataTable();
+            List<DataTable> dataTables = new List<DataTable>();
 
             try
             {
@@ -153,38 +207,53 @@ namespace Spectr.ViewModel
                 {
                     await con.OpenAsync();
 
-                    using (SqlCommand command = new SqlCommand("SELECT * FROM Employer", con))
-                    using (SqlDataReader reader = await command.ExecuteReaderAsync())
+                    using (SqlCommand command = new SqlCommand("SELECT EmployerID as 'ID', CONCAT(EmSecondName, ' ', LEFT(EmFirstName, 1) + '.') AS 'Работник', " +
+                        "PhoneNumber as 'Номер телефона', Salary as 'Зарплата', " +
+                        "Employer.PositionID as 'ID', PositionName as 'Должность'" +
+                        "FROM Employer " +
+                        "Join EmployerPosition ON EmployerPosition.PositionID = Employer.PositionID", con))
+                    
+                    using (SqlDataReader reader1 = await command.ExecuteReaderAsync())
                     {
-                        while (await reader.ReadAsync())
-                        {
-                            Employer employer = new Employer
-                            {
-                                EmployerID = reader.GetInt32(reader.GetOrdinal("EmployerID")),
-                                EmFirstName = reader.GetString(reader.GetOrdinal("EmFirstName")),
-                                EmSecondName = reader.GetString(reader.GetOrdinal("EmSecondName")),
-                                PhoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber")),
-                                Salary = reader.GetDecimal(reader.GetOrdinal("Salary")),
-                                PositionID = reader.GetInt32(reader.GetOrdinal("PositionID")),
+                        DataTable dataTable1 = new DataTable();
+                        dataTable1.Load(reader1);
+                        dataTables.Add(dataTable1);
+                    }
+                    
+                    using (SqlCommand command = new SqlCommand("SELECT CustomerID as 'ID', DocNumber as 'Документ', CONCAT(CustomerSecondName, ' ', LEFT(CustomerFirstName, 1) + '.', " +
+                        "CASE WHEN LEN(CustomerPatronymic) > 0 THEN CONCAT(' ', LEFT(CustomerPatronymic, 1), '.') ELSE '' END) AS 'Клиент' FROM Customer; ", con))
+                    
+                    using (SqlDataReader reader2 = await command.ExecuteReaderAsync())
+                    {
+                        DataTable dataTable2 = new DataTable();
+                        dataTable2.Load(reader2);
+                        dataTables.Add(dataTable2);
+                    }
 
-
-                            };
-                            employers.Add(employer);
-                        }
+                    using (SqlCommand command = new SqlCommand("SELECT DeviceID as 'ID', SerialNumber as 'Серийный номер' FROM Device; ", con))
+                    
+                    using (SqlDataReader reader3 = await command.ExecuteReaderAsync())
+                    {
+                        DataTable dataTable3 = new DataTable();
+                        dataTable3.Load(reader3);
+                        dataTables.Add(dataTable3);
                     }
                 }
             }
+
             catch (Exception ex)
             {
                 MessageBox.Show($"Ошибка отображения: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
-            return employers;
+            return dataTables;          
         }
+      
         #endregion
 
+
         #region Добавление данных
-        
+
         // 1. Открыть окно с добалением
         // 2. Добавить информацию в базу
         public ICommand OpenAddOrderViewCommand { get; }
@@ -199,32 +268,29 @@ namespace Spectr.ViewModel
             var window = new AddOrderView();
             window.ShowDialog();
         }
+
+
+
         #endregion
 
-
-
-
-
-
-
-
-
-
-
+        /// <summary>
+        /// ToDO:
+        /// </summary>
 
 
 
 
         public OrderViewModel()
         {
-            Orders = new ObservableCollection<RepairOrder>();
+            Orders = new ObservableCollection<RepairOrder>();            
 
-
-            _ = EmLoadDataAsync();
- 
             LoadDataRepairOrder();
 
             OpenAddOrderViewCommand = new LambdaCommand(OnOpenAddOrderViewCommandExecuted, CanOpenAddOrderViewCommandExecute);
+
+            _ = LoadAllDataAsync();
+
+
 
 
         }
